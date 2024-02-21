c2b172044a6ebe5e0543e47a85f049f3972105a9
ssssangmin <ksm980609@naver.com>
2024-02-22 03:24:05 +0900

    2/22
    .

--- HollowKnight/Assets/Scripts/Player/PlayerController.cs
+++ HollowKnight/Assets/Scripts/Player/PlayerController.cs
@@ -1,107 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 // 12/31일 수정사항
 // 착지 모션 수정
 
-#pragma region
+#region 1
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rbody;          // RigidBody 2D 컴포넌트
-    float axisH = 0.0f;         // 좌우 방향키 입력
-    public float speed = 3.0f;  // 이동속도
+    [SerializeField] private float speed = 3.0f;  // 이동속도
 
-    public float buttonTime = 1.0f;
-    public float jump = 10.0f;       // 점프력
-    float jumpTime;
-    public LayerMask groundLayer;   // 착지 가능한 레이어
-    bool goJump;            // 점프 키 입력 상태 검사
-    float gravity = -20.0f;
+    [SerializeField] private float jumpTime = 0.5f;
+    [SerializeField] private float jump = 12.0f;       // 점프력
 
-    bool onGround = false;          // 지면과 접촉 상태 검사
+    [SerializeField] private float extraHeight = 0.25f;
+    [SerializeField] private LayerMask groundLayer;   // 착지 가능한 레이어
 
-    bool backIdle = false;
+    private float moveInput;
 
-    Animator animator;
+    private Rigidbody2D rbody;          // RigidBody 2D 컴포넌트
+    private Collider2D coll;
+
+    private Animator animator;
+
+    private RaycastHit2D rayhit;
+
+    private bool isJumping;
+    private bool isFalling;
+    private float jumpTimeCounter;
+    private bool backIdle = false;
+
+    private Coroutine resetTriggerCoroutine;
 
     private void Start()
     {
+        rbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
 
-        rbody = this.GetComponent<Rigidbody>();
+        groundLayer = LayerMask.GetMask("Ground");
     }
 
     private void Update()
     {
-        axisH = Input.GetAxisRaw("Horizontal");
-        // 캐릭터 방향 조절
-        if (axisH > 0.0f)
-        {
-            // 오른쪽 이동
-            Debug.Log("오른쪽 이동");
-            transform.localScale = new Vector2(1, 1);
-        }
-        else if (axisH < 0.0f)
-        {
-            Debug.Log("왼쪽 이동");
-            transform.localScale = new Vector2(-1, 1); // 좌우 반전
-        }
-
-        if (Input.GetButtonUp("Horizontal") && axisH == 0.0f)
-        {
-            animator.SetBool("HorizontalKeyDown", false);
-            RunToIdle();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            goJump = true;
-            jumpTime = 0;
-            animator.Play("Jump");
-        }
-        if (goJump)
-        {
-            jumpTime += Time.deltaTime;
-        }
-        if (Input.GetKeyUp(KeyCode.Z) || jumpTime > buttonTime)
-        {
-            animator.SetTrigger("JumpToFallIdle");
-            goJump = false;
-        }
-
+        Move();
+        Jump();
     }
 
-    private void FixedUpdate()
+    private void Move()
     {
-        // 착지 판정 처리
-        //onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.9f), groundLayer);
+        // UserInput 클래스 인스턴스에서 좌우 이동 값 받아오기.
+        moveInput = UserInput.instance.moveInput.x;
 
-        if (axisH != 0)
+        if (moveInput > 0)
         {
-            // 캐릭터가 지면에 있거나 x축 입력 값이 0이 아닐 경우 velocity 변경
-            rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
-            //transform.Translate(new Vector3(axisH * speed * Time.deltaTime, 0, 0));
+            Vector3 rotate = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotate);
+            animator.SetBool("HorizontalKeyDown", true);
+            animator.SetBool("HorizontalKeyUp", false);
+        }
+        else if (moveInput < 0)
+        {
+            Vector3 rotate = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotate);
             animator.SetBool("HorizontalKeyDown", true);
             animator.SetBool("HorizontalKeyUp", false);
         }
 
+        if (Input.GetButtonUp("Horizontal") && moveInput == 0.0f)
+        {
+            animator.SetBool("HorizontalKeyDown", false);
+            RunToIdle();
+        }
+        rbody.velocity = new Vector2(moveInput * speed, rbody.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
         if (backIdle)
         {
-            Debug.Log("runtoidle");
             animator.SetBool("HorizontalKeyUp", true);
             backIdle = false;
         }
-
-        if (goJump)
-        {
-            // 캐릭터가 지면에 있을 때 점프 키를 입력했을 경우
-            Debug.Log("점프!");
-            rbody.velocity = new Vector2(rbody.velocity.x, jump);
-            
-        }
-
-        rbody.velocity = new Vector2(rbody.velocity.x, rbody.velocity.y + gravity * Time.deltaTime);
     }
 
     public void RunToIdle()
@@ -109,17 +90,100 @@ public class PlayerController : MonoBehaviour
         backIdle = true;
     }
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (collision.gameObject.layer == 6)
-    //    {
-    //        animator.SetTrigger("FallToLand");
-    //    }
-    //}
-}
-#pragma endregion
+    private void Jump()
+    {
+        // 점프를 입력했을 때
+        if (UserInput.instance.controls.Jumping.Jump.WasPressedThisFrame() && isGrounded())
+        {
+            animator.SetTrigger("Jump");
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rbody.velocity = new Vector2(rbody.velocity.x, jump);
+        }
+        // 유지중
+        else if (UserInput.instance.controls.Jumping.Jump.IsPressed())
+        {
+            // 점프를 누르고 있는 시간 계산
+            if (jumpTimeCounter > 0 && isJumping)
+            {
+                rbody.velocity = new Vector2(rbody.velocity.x, jump);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else if (jumpTimeCounter == 0)
+            {
+                isFalling = true;
+                isJumping = false;
+            }
+            // 버튼을 누르고 있을 수 있는 최대 시간이 지나면
+            else
+            {
+                // 추락 애니메이션 재생
+                animator.SetTrigger("JumpToFallIdle");
+                isJumping = false;
+            }
+        }
+        // 점프 버튼에서 손 떼었을 때
+        if (UserInput.instance.controls.Jumping.Jump.WasReleasedThisFrame())
+        {
+            animator.SetTrigger("JumpToFallIdle");
+            isJumping = false;
+            isFalling = true;
+        }
+        if (!isJumping && CheckForLand())
+        {
+            animator.SetTrigger("FallToLand");
+            resetTriggerCoroutine = StartCoroutine(Reset());
+        }
+    }
 
-#pragma region version1
+    // 땅 체크
+    private bool isGrounded()
+    {
+        rayhit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, extraHeight, groundLayer);
+
+        if (rayhit.collider != null)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    private bool CheckForLand()
+    {
+        if (isFalling)
+        {
+            if (isGrounded())
+            {
+                // 땅에 착륙
+                isFalling = false;
+
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private IEnumerator Reset()
+    {
+        yield return null;
+        animator.ResetTrigger("JumpToFallIdle");
+    }
+}
+#endregion
+
+#region
 //public class PlayerController : MonoBehaviour
 //{
 //    Rigidbody2D rbody;          // RigidBody 2D 컴포넌트
@@ -283,4 +347,4 @@ public class PlayerController : MonoBehaviour
 
 //    }
 //}
-#pragma endregion
+#endregion
